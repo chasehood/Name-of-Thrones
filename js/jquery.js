952cc2b1ba2b4b0f37d6052a41aaf19193a448f4
CHASE <t.chasepettyjohn@yahoo.com>
2017-05-30 09:34:23 -0600

    html and css complete

--- js/jquery.js
+++ js/jquery.js
@@ -7,5 +7,7 @@
         $(".paw3").delay(2000).show(1000);
         $(".paw2").delay(1500).show(1000);
         $(".paw1").delay(2500).show(1000);
+        $("p").hide();
+        $("p2").show();
     });
   });
